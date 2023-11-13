2855f23d9b029bb03be9047dae8d2b898fa249fc
Murat Babayiğit <babayigit@gmail.com>
2023-11-13 23:26:39 +0300

    20231113

--- src/test/java/test/C11_POST_ExpectedBody.java
+++ src/test/java/test/C11_POST_ExpectedBody.java
@@ -1,12 +1,14 @@
 package test;
 
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class C11_POST_ExpectedBody {
     /*
@@ -54,34 +56,25 @@ public class C11_POST_ExpectedBody {
         innerDate.put("checkin","2021-06-01");
         innerDate.put("checkout","2021-06-10");
 
-        JSONObject innerPersonal=new JSONObject();
-        innerPersonal.put("firstname","Ahmet");
-        innerPersonal.put("lastname","Bulut");
-        innerPersonal.put("totalprice",500);
-        innerPersonal.put("depositpaid",false);
-        innerPersonal.put("bookingdates",innerDate);
-        innerPersonal.put("additionalneeds","wi-fi");
+        JSONObject reqBody=new JSONObject();
+        reqBody.put("firstname","Ahmet");
+        reqBody.put("lastname","Bulut");
+        reqBody.put("totalprice",500);
+        reqBody.put("depositpaid",false);
+        reqBody.put("bookingdates",innerDate);
+        reqBody.put("additionalneeds","wi-fi");
 
-        JSONObject reqBody = new JSONObject();
-        reqBody.put("bookingid",24);
-        reqBody.put("booking",innerPersonal);
+
 
     //2-
-        JSONObject innerDates=new JSONObject();
-        innerDates.put("checkin","2021-06-01");
-        innerDates.put("checkout","2021-06-10");
-
-        JSONObject innerPers=new JSONObject();
-        innerPers.put("firstname","Ahmet");
-        innerPers.put("lastname","Bulut");
-        innerPers.put("totalprice",500);
-        innerPers.put("depositpaid",false);
-        innerPers.put("bookingdates",innerDates);
-        innerPers.put("additionalneeds","wi-fi");
 
         JSONObject expBody = new JSONObject();
         expBody.put("bookingid",24);
-        expBody.put("booking",innerPers);
+        expBody.put("booking",reqBody); // beklentimin gönderdiğim ile gelenin aynısı olması olduğu için expData=reqBody
+
+
+
+
 
     //3-
         Response response=given()
@@ -89,7 +82,20 @@ public class C11_POST_ExpectedBody {
                                 .when()
                                     .body(reqBody.toString()).post(url);
 
+
+
+
+
     //4-
+        JsonPath resJP=response.jsonPath();
+
+        assertEquals(expBody.getJSONObject("booking").get("firstname"),resJP.get("booking.firstname"));
+        assertEquals(expBody.getJSONObject("booking").get("lastname"),resJP.get("booking.lastname"));
+        assertEquals(expBody.getJSONObject("booking").get("totalprice"),resJP.get("booking.totalprice"));
+        assertEquals(expBody.getJSONObject("booking").get("depositpaid"),resJP.get("booking.depositpaid"));
+        assertEquals(expBody.getJSONObject("booking").get("additionalneeds"),resJP.get("booking.additionalneeds"));
+        assertEquals(expBody.getJSONObject("booking").getJSONObject("bookingdates").get("checkin"),resJP.get("booking.bookingdates.checkin"));
+        assertEquals(expBody.getJSONObject("booking").getJSONObject("bookingdates").get("checkout"),resJP.get("booking.bookingdates.checkout"));
 
 
 
