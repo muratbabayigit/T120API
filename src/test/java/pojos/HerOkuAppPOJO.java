674a424403e25133f49ef3db5d166d27ee6183e6
Murat Babayiğit <babayigit@gmail.com>
2023-11-17 23:20:27 +0300

    API 20231117

---
+++ src/test/java/pojos/HerOkuAppPOJO.java
@@ -0,0 +1,30 @@
+package pojos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HerOkuAppPOJO {
+
+/*
+Response Body = Expected Data
+    	            	{
+                    "bookingid":24,
+                    "booking":{
+                        "firstname":"Ali",
+                        "lastname":"Bak",
+                        "totalprice":500,
+                        "depositpaid":false,
+                        "bookingdates":{
+                            "checkin":"2021-06-01",
+                            "checkout":"2021-06-10"
+                                        }
+ */
+    private int bookingid;
+    private BookingPOJO booking;
+
+}
--- src/test/java/test/C24_POST_POJOClas.java
+++ src/test/java/test/C24_POST_POJOClas.java
@@ -1,9 +1,16 @@
 package test;
 
 import baseUrl.HerOkuAppBaseURL;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 import pojos.BookingDatesPOJO;
 import pojos.BookingPOJO;
+import pojos.HerOkuAppPOJO;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
 
 public class C24_POST_POJOClas extends HerOkuAppBaseURL {
     /*
@@ -47,6 +54,27 @@ public class C24_POST_POJOClas extends HerOkuAppBaseURL {
         specHerOkuApp.pathParam("pp1","booking");
         BookingDatesPOJO bookingdates=new BookingDatesPOJO("2021-06-01","2021-06-10");
         BookingPOJO reqBody=new BookingPOJO("Ali","Bak",500,false,bookingdates,"wi-fi");
+
+        HerOkuAppPOJO expBody=new HerOkuAppPOJO(1364,reqBody);
+
+        Response response=given()
+                                .spec(specHerOkuApp)
+                                .contentType(ContentType.JSON)
+                                .when().body(reqBody)
+                                .post("/{pp1}");
+
+         HerOkuAppPOJO respPOJO=response.as(HerOkuAppPOJO.class);
+
+        //assertEquals(expBody.getBookingid(),respPOJO.getBookingid());
+        assertEquals(expBody.getBooking().getFirstname(),respPOJO.getBooking().getFirstname());
+        assertEquals(expBody.getBooking().getLastname(),respPOJO.getBooking().getLastname());
+        assertEquals(expBody.getBooking().getTotalprice(),respPOJO.getBooking().getTotalprice());
+        assertEquals(expBody.getBooking().isDepositpaid(),respPOJO.getBooking().isDepositpaid());
+        assertEquals(expBody.getBooking().getBookingdates().getCheckin(),respPOJO.getBooking().getBookingdates().getCheckin());
+        assertEquals(expBody.getBooking().getBookingdates().getCheckout(),respPOJO.getBooking().getBookingdates().getCheckout());
+        assertEquals(expBody.getBooking().getAdditionalneeds(),respPOJO.getBooking().getAdditionalneeds());
+
+
 
 
 
